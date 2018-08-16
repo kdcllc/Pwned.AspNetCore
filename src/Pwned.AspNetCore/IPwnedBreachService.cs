33b094a1e1f1267fc6ecba70799b5044b3280a3b
kdcllc <info@kingdavidconsulting.com>
2018-08-15 23:43:49 -0400

    add interfaces

---
+++ src/Pwned.AspNetCore/IPwnedBreachService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Pwned.AspNetCore.Models;
+
+namespace Pwned.AspNetCore
+{
+    public interface IPwnedBreachService
+    {
+        HttpClient Client { get; }
+
+        Task<List<Breach>> GetAllBreachesAsync(string domain = "", CancellationToken token = default);
+        Task<List<string>> GetAllDataClasses(CancellationToken token = default);
+        Task<List<Breach>> GetBreachesByAccountAsync(string emailAccount, bool unVerified = false, bool fullResponse = true, string domain = "", CancellationToken token = default);
+        Task<List<PastAccount>> GetPastByAccount(string emailAccount, CancellationToken token = default);
+    }
+}
---
+++ src/Pwned.AspNetCore/IPwnedPasswordService.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+using System.Threading;
+
+namespace Pwned.AspNetCore
+{
+    public interface IPwnedPasswordService
+    {
+        HttpClient Client { get; }
+
+        System.Threading.Tasks.Task<(bool pwned, long count)> IsPasswordPwned(string password, CancellationToken token = default);
+    }
+}
